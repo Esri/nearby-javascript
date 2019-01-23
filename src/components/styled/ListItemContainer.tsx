b3d16de39cea46a4b3deb3389f4d6f2d6131f00d
odoe <rrubalcava@esri.com>
2019-01-23 07:52:15 -0800

    try to remove ripple for ios devices per #3

--- src/components/styled/ListItemContainer.tsx
+++ src/components/styled/ListItemContainer.tsx
@@ -5,6 +5,20 @@ const ListItemContainer = styled(ListItem)`
   cursor: pointer;
   span > small {
     vertical-align: top;
+  },
+  @media only screen 
+  and (min-device-width : 375px) 
+  and (max-device-width : 812px) 
+  and (-webkit-device-pixel-ratio : 3)
+  and (orientation : portrait) {
+    --mdc-ripple-fg-size: 0;
+    --mdc-ripple-left: 0;
+    --mdc-ripple-top: 0;
+    --mdc-ripple-fg-scale: 1;
+    --mdc-ripple-fg-translate-end: 0;
+    --mdc-ripple-fg-translate-start: 0;
+    -webkit-tap-highlight-color: rgba(0, 0, 0, 0);
+    will-change: transform, opacity;
   }
 `;
 
