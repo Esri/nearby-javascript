d35fdd98a77ad15cc14cfb66bc2ef70fecfad717
odoe <rrubalcava@esri.com>
2019-01-25 08:39:23 -0800

    check for geolocation support, fixes #18

--- src/hooks/useGeolocation.ts
+++ src/hooks/useGeolocation.ts
@@ -14,6 +14,13 @@ const useGeolocation = () => {
   let mounted = true;
 
   const locate = (latLon?: LatLon) => {
+    // If geolocation not supported in the
+    // browser, set lat/lon to 0
+    // will be handled in application context
+    if (!("geolocation" in navigator)) {
+      setState({ latitude: 0, longitude: 0 });
+      return;
+    }
     if (latLon) {
       setState(latLon);
     } else {
